920fcde83e8eb17cdc523ce7f89c0e767d177ab6
Eric Stratigakis <enstrati@uwaterloo.ca>
2021-07-28 13:48:55 -0400

    small change

--- components/Findings/FindingsReportController.js
+++ components/Findings/FindingsReportController.js
@@ -9,6 +9,7 @@ const FindingsReportController = () => {
           "Stage 01 - Submit Record",
           "Stage 02 - Review Record",
           "Stage 03 - Accept Record",
+          "Stage 04- Accept Record",
         ]}
       />
     </div>
