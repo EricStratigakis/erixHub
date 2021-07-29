dccfbcbdf3c2ee5adcab890cb9113c8a994c9567
Eric Stratigakis <enstrati@uwaterloo.ca>
2021-07-29 10:00:59 -0400

    small change

--- components/Findings/FindingsReportController.js
+++ components/Findings/FindingsReportController.js
@@ -2,17 +2,15 @@ import FindingsFilterList from "./FindingsFilterList";
 
 const FindingsReportController = () => {
   return (
-    <div>
-      <FindingsFilterList
-        filterParam="Stage Stauts"
-        options={[
-          "Stage 01 - Submit Record",
-          "Stage 02 - Review Record",
-          "Stage 03 - Accept Record",
-          "Stage 04- Accept Record",
-        ]}
-      />
-    </div>
+    <FindingsFilterList
+      filterParam="Stage Stauts"
+      options={[
+        "Stage 01 - Submit Record",
+        "Stage 02 - Review Record",
+        "Stage 03 - Accept Record",
+        "Stage 04- Accept Record",
+      ]}
+    />
   );
 };
 
