0ed60feaf1741232026eb865f01ed254be77e344
Eric Stratigakis <enstrati@uwaterloo.ca>
2021-07-29 10:04:04 -0400

    deleted file

--- components/Findings/FindingsReportController.js
+++ components/Findings/FindingsReportController.js
@@ -1,16 +1,18 @@
-import FindingsFilterList from "./FindingsFilterList";
+// import FindingsFilterList from "./FindingsFilterList";
 
 const FindingsReportController = () => {
   return (
-    <FindingsFilterList
-      filterParam="Stage Stauts"
-      options={[
-        "Stage 01 - Submit Record",
-        "Stage 02 - Review Record",
-        "Stage 03 - Accept Record",
-        "Stage 04- Accept Record",
-      ]}
-    />
+    <div>
+      {/* <FindingsFilterList
+        filterParam="Stage Stauts"
+        options={[
+          "Stage 01 - Submit Record",
+          "Stage 02 - Review Record",
+          "Stage 03 - Accept Record",
+          "Stage 04- Accept Record",
+        ]}
+      /> */}
+    </div>
   );
 };
 
